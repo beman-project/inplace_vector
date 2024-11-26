3ec26009283a19727065afc1305b816899d86560
River <26424577+wusatosi@users.noreply.github.com>
2024-11-25 22:41:04 -0500

    Update include/beman/inplace_vector/inplace_vector.hpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- include/beman/inplace_vector/inplace_vector.hpp
+++ include/beman/inplace_vector/inplace_vector.hpp
@@ -69,8 +69,9 @@ struct inplace_vector_type_based_storage {
   constexpr const T *begin() const { return elems.data(); }
 };
 
-// byte based storage is used for non-constexpr environment. Objects of type T are only
-// constructed when needed and they don't even have to be default constructible. 
+// byte based storage is used for non-constexpr environment. Objects of type T
+// are only constructed when needed and they don't even have to be default
+// constructible.
 //
 // Selecting this storage type implies: !std::is_trivial_v<T> and Capacity != 0
 template <typename T, std::size_t Capacity>
